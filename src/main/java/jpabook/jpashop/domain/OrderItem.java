11d3127c76ad08809e10cc2b22ac0f86df5c734b
heeseong <hsung0823@hanmail.net>
2021-10-21 19:53:48 +0900

    * 지연로딩 설정 * 연관관계 편의 메소드 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -6,16 +6,18 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "item_id")
     private Item item;
-    @ManyToOne
+    @ManyToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "order_id")
     private Order order;
     private int orderPrice;
