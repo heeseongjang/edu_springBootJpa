688e9c2c391ecd4706b3811af5500caac18687b2
heeseong <hsung0823@hanmail.net>
2021-10-24 13:23:01 +0900

    * 주문, 주문상품 엔티티 및 레파지토리 개발(도메인 모델 패턴)

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
 
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -23,7 +26,6 @@ public class OrderItem {
     private Order order;
     private int orderPrice;
     private int count;
-
 
     //생성 메서드
     public static OrderItem createOrderItem(Item item, int orderPrice, int count){
