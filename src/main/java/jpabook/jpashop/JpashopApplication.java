539690c0f5def064a5f6441e32f4b01ee3c396aa
heeseong <wkdgmltjd1865@gmail.com>
2021-11-03 22:19:26 +0900

    * 간단한 주문 조회 - v1 @JsonIgnore

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //패키지 안에 있는 모든것을 component scan해서 spring bean으로 자동 등록
@@ -11,4 +13,8 @@ public class JpashopApplication {
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module(){
+		return new Hibernate5Module();
+	} // 단점 : 엔티티 노출된다.
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -20,7 +21,7 @@ public class Member {
     private String name;
     @Embedded //내장을 하였다.
     private Address address;
-    //    @JsonIgnore
+    @JsonIgnore
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
