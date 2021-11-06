380e201f95ecbfe9ca5d2526529535ff6755da8c
heeseong <wkdgmltjd1865@gmail.com>
2021-11-06 20:53:35 +0900

    * 주문 조회 - v5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -69,10 +69,14 @@ public class OrderApiController {
     }
 
     @GetMapping("/api/v4/orders")
-    public List<OrderQueryDto> ordersV4(){
-return orderQueryRepository.findOrderQueryDtos();
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
     @Getter
     static class OrderDTO {
         private Long orderId;
